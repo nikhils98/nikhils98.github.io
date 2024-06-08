c8e248f53fda725a561e080e91636bc54328e5af
Nikhil Satiani <nikhil.satiani@gmail.com>
2024-06-08 17:47:49 +0500

    upgrade all packages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'handwriting': ['Dancing Script', 'Allison', 'cursive']
-			}
-		},
-	},
-	plugins: [
-		require('@tailwindcss/typography')
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        handwriting: ["Dancing Script", "Allison", "cursive"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
